79ef7c048e8601f05c56d25aa344aae8739f61fd
khangdev <khang.s090447@ilamail.edu.vn>
2024-10-20 19:29:04 +0700

    js2

---
+++ test/ss2.js
@@ -0,0 +1,20 @@
+console.log("Hello World");
+
+let age = 25;
+
+let yourFirstName = 'Hồ Phú Khang';
+
+
+console.log(age)
+console.log(yourFirstName)
+const Pi = 111;
+
+console.log(`Em tên là ${yourFirstName}, Em năm nay ${age}.`)
+
+let data = 'Thiên. Lý. ơi. em. có. thể. ở. lại. đây. không?';
+let subStrings = data.split('.');
+console.log(subStrings)
+let result = yourFirstName + data; 
+console.log(result)
+
+
